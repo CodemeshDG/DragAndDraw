b3c807fa687611609637c9ced3fa066651423ef1
CodemeshDG <DomG1984@hotmail.com>
2019-10-01 13:46:23 -0400

    Initial commit

---
+++ app/src/main/java/com/dommyg/draganddraw/BoxDrawingView.java
@@ -0,0 +1,89 @@
+package com.dommyg.draganddraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
+    private Box currentBox;
+    private List<Box> boxes = new ArrayList<>();
+    private Paint boxPaint;
+    private Paint backgroundPaint;
+
+    // Used when creating the view in code.
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML.
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Paint the boxes a nice semitransparent red (ARGB).
+        boxPaint = new Paint();
+        boxPaint.setColor(0x22ff0000);
+
+        // Paint the background off-white.
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Fill the background.
+        canvas.drawPaint(backgroundPaint);
+
+        for (Box box : boxes) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, boxPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                // Reset drawing state.
+                currentBox = new Box(current);
+                boxes.add(currentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                if (currentBox != null) {
+                    currentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                currentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                currentBox = null;
+                break;
+        }
+
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+
+        return true;
+    }
+}
